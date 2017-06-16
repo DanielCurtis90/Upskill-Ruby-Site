59e9cf80fa87bd07223b1030928101e65ebe6eae
Daniel Curtis <danielcurtis0@gmail.com>
2017-06-16 23:33:59 +0000

    cleaned up routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
-  get 'contact-us', to: 'contacts#new' #This is to make a custom URL, routing to the new method of the contacts controller
+  resources :contacts, only: :create #If limiting more than one, enter them in an array ie: [:new, :create, :etc]
+  get 'contact-us', to: 'contacts#new', as: 'new_contact' #This is to make a custom URL, routing to the 'new' method of the contacts controller
 end
