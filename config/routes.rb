c74c5027a751a08f0e46b96ca3a0f9c47a916cee
Daniel Curtis <danielcurtis0@gmail.com>
2017-06-16 00:27:36 +0000

    Added custom url for contact us

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts
+  get 'contact-us', to: 'contacts#new' #This is to make a custom URL, routing to the new method of the contacts controller
 end
