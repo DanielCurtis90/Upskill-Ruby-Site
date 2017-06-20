efb27afef1798d4e8931cf99fca6064c72424195
Daniel Curtis <danielcurtis0@gmail.com>
2017-06-20 00:57:04 +0000

    Comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,12 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params) #[error1, error2, ..] from contact_params
     if @contact.save
-      name = params[:contact][:name] #This lifts the information entered on the form before it's sent to the database
-      email = params[:contact][:email]
-      body = params[:contact][:comments]
+      #This lifts the information entered on the form before it's sent to the database
+      #The information is grabbed from the Parameters hash that is sent when submit is pressed
+      # :contact refers to one of the keys in the Parameters hash, :name etc refers to the next key pointing to our form data
+      name = params[:contact][:name] 
+      email = params[:contact][:email] 
+      body = params[:contact][:comments] 
       ContactMailer.contact_email(name, email, body).deliver #This uses the delivermethod from the ActiveMailer that ContactMailer inherhits from
       flash[:success] = "Message sent." #The keys for the flashes match the bootstrap format to save time
       redirect_to new_contact_path
