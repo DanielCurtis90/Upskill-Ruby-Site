651a2c51677c569883b7e3e37dd69c7658b9ed88
Daniel Curtis <danielcurtis0@gmail.com>
2017-07-21 01:55:06 +0000

    Added flash message and redirect in registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :select_plan, only: new
+  
   # Extend default Devise gem behavior so that users signing up with the pro
   # account (plan id 2) save with a special stripe subscription function.
   # Otherwise Devise signs up user as usual.
@@ -15,5 +17,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
-
+  private
+    def select_plan
+      unless (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
+      end
+    end
 end
