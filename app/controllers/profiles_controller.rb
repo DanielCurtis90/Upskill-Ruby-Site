d8cb345bad8e2b0ac85c5bc5a9001eb287e671b5
Daniel Curtis <danielcurtis0@gmail.com>
2017-07-26 22:55:14 +0000

    Added profile creation form and controller details, added relationship between user and built profile objects

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,29 @@
 class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
   def new
-    # Render blank profile details form
+    @profile = Profile.new
+  end
+  
+  # POST to /users/:user_id/profile
+  def create
+  # Ensure that we have the user who is filling out form
+  @user = User.find( params[:user_id] )
+  # Create profile linked to this specific user
+  # The user object has this build method due to the association between it and
+  # the profile object
+  # Use .build instead of .new when you want a relationship between the two
+  # objects!!!
+  @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+  
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
   end
 end
