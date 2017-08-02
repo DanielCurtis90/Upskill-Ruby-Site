a1a0db7e2cdb9cfe8fc6a76b364c77b335723ec7
Daniel Curtis <danielcurtis0@gmail.com>
2017-08-02 01:21:56 +0000

    Added security to user pages and editing, index route and view added

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,11 @@
 class ProfilesController < ApplicationController
+  # This is from Devise. It checks that there is a user logged in to run these
+  # actions. Note that before actions can be limited i.e only [:new]
+  before_action :authenticate_user!
+  # We need to define this one down below in Private
+  before_action :only_current_user
+  
+  
   # GET to /users/:user_id/profile/new
   def new
     @profile = Profile.new
@@ -18,7 +25,7 @@ class ProfilesController < ApplicationController
       flash[:success] = "Profile updated!"
       # The user_path uses :id, however the query string parameter from the 
       # profiles page is using :user_id, so that's where the parameter is found
-      redirect_to user_path( params[:user_id] )
+      redirect_to user_path(id: params[:user_id] )
     else
       render action: :new
     end
@@ -31,8 +38,33 @@ class ProfilesController < ApplicationController
     @user = User.find( params[:user_id] )
     @profile = @user.profile
   end  
-  private
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+  
+  # PUT to /users/:user_id/profile
+  def update
+    # Retrieve the user from the database
+    # Grab the user from the ID in the URL, then stick their profile data 
+    # into the form
+    @user = User.find( params[:user_id] )
+    # Retrieve that users profile into the form fields
+    @profile = @user.profile
+    # Mass assign edited profile attributes and save (update)
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      # Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
   end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+  
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      # Move them to the home page if they aren't they dont match the profile ID
+      redirect_to(root_url) unless @user == current_user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  # This is from Devise. It checks that there is a user logged in to run these
+  # actions. We dont want non-users looking at users!
+  before_action :authenticate_user!
+  
+  def index
+    
+  end
+  
   # GET to /users/:id
   # Note that it's now :id instead of :user_id that we used in the nested routes
   def show
